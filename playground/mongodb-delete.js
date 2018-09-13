4ba98ad3abcf9534f31404307473967371f06965
Manish Arora <manish.arora1392@gmail.com>
2018-09-12 18:47:39 -0700

    adding mongodb-delete.js

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,37 @@
+const {MongoClient,ObjectID}  = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017',(err,client)=>{
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    //delete Many
+
+    // db.collection('Todos').deleteMany({text:'Eat lunch'}).then((result)=>{
+    //     console.log(result);
+    // });
+
+    
+    //delete One
+    // db.collection('Todos').deleteOne({text:'Eat lunch'}).then((result)=>{
+    //     console.log(result);
+    // });
+
+
+    //find One and delete
+    // db.collection('Todos').findOneAndDelete({completed:false}).then((result)=>{
+    //     console.log(result);
+    // });
+     
+    db.collection('Users').deleteMany({name:'Manish'});
+
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID("5b91dae0f6746a3a505fd4e1")
+    }).then((result)=>{
+        console.log(JSON.stringify(result,undefined,2));
+    });
+    //client.close()
+});
