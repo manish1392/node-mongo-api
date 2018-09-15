e8db069c8ef866e1b9cd1496eb0ff0d9fb208b98
Manish Arora <manish.arora1392@gmail.com>
2018-09-15 06:34:37 -0700

    Validation to mongoose model

--- server/server.js
+++ server/server.js
@@ -5,36 +5,49 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 var Todo = mongoose.model('Todo',{
     text:{
-        type:String
+        type:String,
+        required:true,
+        minlength:1,
+        trim:true
     },
     completed:{
-        type:Boolean
+        type:Boolean,
+        default:false
     },
     completedAt:{
-        type:Number
+        type:Number,
+        default:null
     }
 });
 
-
-// var newTodo = new Todo({
-//     text:'Cook Dinner'
+// var otherTodo = new Todo({
+//     text:'  Testing defaults  ',
+//     // completed:true,
+//     // completedAt:123
 // });
- 
 
-// newTodo.save().then((doc)=>{
-//     console.log('Saved Todo',doc);
+// otherTodo.save().then((doc)=>{
+//     console.log(JSON.stringify(doc,undefined,2));
 // },(e)=>{
 //     console.log('Unable to save todo',e);
 // });
 
-var otherTodo = new Todo({
-    text:'Feed the cat',
-    completed:true,
-    completedAt:123
+
+var User = mongoose.model('User',{
+    email:{
+        type:String,
+        required:true,
+        trim:true,
+        minlength:1
+    }
+}); 
+
+var user = new User({
+    email:'manish@example.com   '
 });
 
-otherTodo.save().then((doc)=>{
+user.save().then((doc)=>{
     console.log(JSON.stringify(doc,undefined,2));
 },(e)=>{
-    console.log('Unable to save todo',e);
-});
+    console.log('Unable to save user',e);
+})
